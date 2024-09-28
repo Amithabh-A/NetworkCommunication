ae1bac2cf97ee3776250e6885cbe05f946c64303
Amithabh-A <amithabh2002@gmail.com>
2024-09-28 21:08:52 +0530

    Initial version of .dll sending applications

---
+++ Client/Client/Client.cs
@@ -0,0 +1,35 @@
+﻿// sender
+
+using System;
+using System.IO;
+using System.Net.Sockets;
+namespace Client;
+
+public class Client
+{
+  static void Main(string[] args)
+  {
+    string serverIP = args[0]; // Replace with the server's IP address
+    int port = int.Parse(args[1]); // Same port as the server
+
+    TcpClient client = new TcpClient(serverIP, port);
+    Console.WriteLine("Connected to the server.");
+
+    using (NetworkStream stream = client.GetStream())
+    {
+      using (FileStream fileStream = new FileStream(args[2], FileMode.Open, FileAccess.Read))
+      {
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+        {
+          stream.Write(buffer, 0, bytesRead);
+        }
+      }
+    }
+
+    client.Close();
+    Console.WriteLine("File sent to the server.");
+  }
+}
+
---
+++ Server/Server/Server.cs
@@ -0,0 +1,41 @@
+﻿// Receiver
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+namespace Server;
+
+public class Server
+{
+  static void Main(string[] args)
+  {
+    int port = int.Parse(args[0]); // Define the port number
+
+    TcpListener server = new TcpListener(IPAddress.Any, port);
+    server.Start();
+    Console.WriteLine("Server started. Waiting for connection...");
+
+    using (TcpClient client = server.AcceptTcpClient())
+    {
+      Console.WriteLine("Client connected!");
+
+      using (NetworkStream stream = client.GetStream())
+      {
+        using (FileStream fileStream = new FileStream("received.dll", FileMode.Create, FileAccess.Write))
+        {
+          byte[] buffer = new byte[1024];
+          int bytesRead;
+          while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+          {
+            fileStream.Write(buffer, 0, bytesRead);
+          }
+        }
+      }
+    }
+
+    server.Stop();
+    Console.WriteLine("File received and saved as 'received.dll'");
+  }
+}
+
